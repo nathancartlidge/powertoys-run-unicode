1b98006fed10254225bbd31c08dee2a4a0116ec3
nathancartlidge <11426516+nathancartlidge@users.noreply.github.com>
2024-03-16 17:12:38 +0000

    feat: basic project structure
    This successfully loads inside Agda and returns the '≡' symbol to the clipboard!
    
    This is based upon https://github.com/skttl/ptrun-guid and https://github.com/naari3/PowerToysRunPluginSample/tree/main

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Agda (latex) input for Powertoys Run
 
-# generator for PowerToys Run
-Allows you to type `\==` into PowerToys run and get `≡` in the clipboard.
+A small script that allows you to type `\==` into PowerToys run and get `≡` in the clipboard.
 
 ## Attribution
 Source code based upon [ptrun-guid](https://github.com/skttl/ptrun-guid) by skttl.
---
+++ src/nthn.Agda/Main.cs
@@ -0,0 +1,61 @@
+﻿using ManagedCommon;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Wox.Plugin;
+
+namespace nthn.Agda
+{
+    public class Main : IPlugin
+    {
+        private string IconPath { get; set; }
+
+        private PluginInitContext Context { get; set; }
+        public string Name => "Agda";
+
+        public string Description => "Agda Unicode Input";
+
+        public static string PluginID => "778f24fc48714097b30303f83d5bed6a";
+
+        public List<Result> Query(Query query)
+        {
+            return new List<Result>
+            {
+                new Result
+                {
+                    Title = "Match found!",
+                    SubTitle = "Copy this symbol to the clipboard",
+                    IcoPath = IconPath,
+                    Action = e =>
+                    {
+                        Clipboard.SetText("≡");
+                        return true;
+                    },
+                }
+            };
+        }
+
+        public void Init(PluginInitContext context)
+        {
+            Context = context;
+            Context.API.ThemeChanged += OnThemeChanged;
+            UpdateIconPath(Context.API.GetCurrentTheme());
+        }
+
+        private void UpdateIconPath(Theme theme)
+        {
+            if (theme == Theme.Light || theme == Theme.HighContrastWhite)
+            {
+                IconPath = "images/agda.light.png";
+            }
+            else
+            {
+                IconPath = "images/agda.dark.png";
+            }
+        }
+
+        private void OnThemeChanged(Theme currentTheme, Theme newTheme)
+        {
+            UpdateIconPath(newTheme);
+        }
+    }
+}
