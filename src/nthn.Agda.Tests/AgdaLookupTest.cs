0bcf8c5f6282b7e3cd8fd3bdfab78b6cc279d5fa
nathancartlidge <11426516+nathancartlidge@users.noreply.github.com>
2024-03-19 02:19:48 +0000

    wip: basic implementation of partial matching (using binary search on sorted list for perf)
    not fully tested, but appears to be relatively stable, so I'll start integrating it

--- src/nthn.Agda.Tests/AgdaLookupTest.cs
+++ src/nthn.Agda.Tests/AgdaLookupTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Printing;
 using JetBrains.Annotations;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,29 +12,29 @@ namespace nthn.Agda.Tests;
 [TestSubject(typeof(AgdaLookup))]
 public class AgdaLookupTest
 {
+    public AgdaLookup Lookup = new AgdaLookup();
 
     [TestMethod]
     public void TextExact()
     {
-        var lookup = new AgdaLookup();
         string[] result;
         
         Console.WriteLine("Testing single matches");
         Console.WriteLine("Testing simple case ('_2' to '₂')");
         
-        result = lookup.ExactMatches("_2");
+        result = Lookup.ExactMatches("_2");
         Assert.AreEqual(result.Length, 1);
         Assert.AreEqual(result[0], "₂");
 
         Console.WriteLine("Testing long case ('^\\turned r with long leg and retroflex hook' to '𐞧')");
         
-        result = lookup.ExactMatches("^\\turned r with long leg and retroflex hook");
+        result = Lookup.ExactMatches("^\\turned r with long leg and retroflex hook");
         Assert.AreEqual(result.Length, 1);
         Assert.AreEqual(result[0], "𐞧");
         
         Console.WriteLine("Testing multiple output (':')");
 
-        result = lookup.ExactMatches(":");
+        result = Lookup.ExactMatches(":");
         // "∶ ⦂ ː ꞉ ˸ ፥ ፦ ： ﹕ ︓  "
         Assert.AreEqual(result.Length, 11);
         Assert.AreEqual(result[0], "\u2236");
@@ -40,30 +42,51 @@ public class AgdaLookupTest
 
         Console.WriteLine("Testing invalid case");
 
-        result = lookup.ExactMatches("thisisnotavalidkey");
+        result = Lookup.ExactMatches("thisisnotavalidkey");
         Assert.AreEqual(result.Length, 0);
     }
 
     [TestMethod]
     public void TestNumber()
     {
-        var lookup = new AgdaLookup();
         string result;
 
         Console.WriteLine("Testing on ':'");
         // "∶ ⦂ ː ꞉ ˸ ፥ ፦ ： ﹕ ︓  "
-        result = lookup.NumberMatch(":4");
+        result = Lookup.NumberMatch(":4");
         Assert.AreEqual(result, "\ua789");
 
         Console.WriteLine("Testing out-of-bounds array indexing");
         
-        result = lookup.NumberMatch(":0");
+        result = Lookup.NumberMatch(":0");
         Assert.AreEqual(result, null);
-        result = lookup.NumberMatch(":100");
+        result = Lookup.NumberMatch(":100");
         Assert.AreEqual(result, null);
         
         Console.WriteLine("Testing invalid case");
-        result = lookup.NumberMatch("_2"); // there is not a '_' options list
+        result = Lookup.NumberMatch("_2"); // there is not a '_' options list
         Assert.AreEqual(result, null);
+    }
+
+    [TestMethod]
+    public void TestPartial()
+    {
+        // todo: more tests are still needed to fully exclude the risk of off-by-one cases!
+        List<char> nextChars;
+        List<string> partialMatches;
+        
+        // G[ABCDEFGHIKLMNOPRSTUXZabcdefghiklmnoprstuxz]
+        (nextChars, partialMatches) = Lookup.PartialMatch("G");
+        var expected = "ABCDEFGHIKLMNOPRSTUXZabcdefghiklmnoprstuxz";
+        foreach (var c in expected)
+        {
+            Assert.IsTrue(nextChars.Contains(c));
+        }
+        Assert.IsTrue(partialMatches.Contains("Gamma"));
+        
+        // invalid
+        (nextChars, partialMatches) = Lookup.PartialMatch("thisisnotavalidkey");
+        Assert.IsTrue(nextChars.Count == 0);
+        Assert.IsTrue(partialMatches.Count == 0);
     }
 }
--- src/nthn.Agda/Lookup.cs
+++ src/nthn.Agda/Lookup.cs
@@ -1,14 +1,18 @@
 // README: this file was generated from `Lookup.template.cs` in `tools/`. Make sure you apply any edits
 //         you make to this file to that as well so the edits can be replicated if you regenerate this
 //         file!
-﻿using System.Collections.Generic;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
 using System.Text.RegularExpressions;
 
 namespace nthn.Agda
 {
     public class AgdaLookup
     {
-        public Dictionary<string, string> KeyValuePairs = new Dictionary<string, string>()
+        public static Dictionary<string, string> KeyValuePairs = new Dictionary<string, string>()
         {
             {"%", "％"},
             {"+", "⊹"},
@@ -2607,6 +2611,61 @@ namespace nthn.Agda
             }
 
             return null;
+        }
+
+        private List<string> _sortedKeys = KeyValuePairs.Keys.ToList().Order(StringComparer.Ordinal).ToList();
+
+        public (List<char>, List<string>) PartialMatch(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return ([], []);
+            }
+            // goal: we want all elements of _sortedKeys that have `key` as a suffix.
+            //       theoretically we can just do this with a filter but I want to be fancy about it
+            //       so, instead I intend to do this with a binary search:
+            //        - look for the location of key
+            //        - look for the location of key+1
+            //       these will be my elements
+            
+            // get the starting point
+            int firstIndex = _sortedKeys.BinarySearch(key, StringComparer.Ordinal);
+            if (firstIndex < 0)
+            {
+                firstIndex = ~ firstIndex;
+            }
+            else
+            {
+                firstIndex += 1;
+            }
+            
+            // get the next element - because the list is sorted, this can just be done by incrementing the last char
+            string allButLast = key.Substring(0, key.Length - 1);
+            char lastChar = key[key.Length - 1];
+            int incrementedCode = lastChar + 1;
+            char incrementedChar = (char) incrementedCode;
+            string subsequentKey = allButLast + incrementedChar;
+
+            int lastIndex = _sortedKeys.BinarySearch(subsequentKey, StringComparer.Ordinal);
+            if (lastIndex < 0)
+            {
+                lastIndex = ~ lastIndex;
+            }
+
+            List<char> validNextChars = new List<char>();
+            List<string> validPartialMatches = new List<string>();
+
+            for (int i = firstIndex; i < lastIndex; i++)
+            {
+                string value = _sortedKeys[i];
+                validPartialMatches.Add(value);
+                if (validNextChars.Count == 0 || value[key.Length] != validNextChars.Last())
+                {
+                    validNextChars.Add(value[key.Length]);
+                }
+            } 
+            
+            return (validNextChars, validPartialMatches);
         }
     }
 }
